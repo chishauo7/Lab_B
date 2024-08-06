75cdd80a9d44416446b095cf1045c67e1efbd2b6
chishauo7 <onoyi17@gmail.com>
2024-08-06 16:04:30 +0800

    新增好康報報

--- js/myScript.js
+++ js/myScript.js
@@ -1,3 +1,14 @@
+var modalElem = document.getElementById("tyModal");
+var spanElem = document.getElementsByClassName("tyClose")[0];
+
+spanElem.onclick = function () {
+    modalElem.style.display = "none";
+}
+
+setTimeout(function () {
+    modalElem.style.display = "block";
+}, 1000);
+
 // Tabs
 function openLink(evt, linkName) {
     var i, x, tablinks;
